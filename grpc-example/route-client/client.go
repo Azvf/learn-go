618ee2273412211fce28d9595bb396ca159d89ba
Azvf <mazhenyu284@gmail.com>
2022-09-12 00:15:42 +0800

    finish the gRPC single sided streaming and bidirectional streaming implementation

--- grpc-example/route-client/client.go
+++ grpc-example/route-client/client.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	pb "github.com/Azvf/learn-go/grpc-example/route"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
+	"os"
+	"time"
 )
 
 func runFirst(client pb.RouteGuideClient) {
@@ -16,6 +20,105 @@ func runFirst(client pb.RouteGuideClient) {
 	}
 
 	fmt.Println(feature)
+}
+
+func runSecond(client pb.RouteGuideClient) {
+	serverStream, err := client.ListFeatures(context.Background(), &pb.Rectangle{
+		Lo: &pb.Point{Longitude: 121358540, Latitude: 313374060},
+		Hi: &pb.Point{Longitude: 121598790, Latitude: 311034130},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		feature, err := serverStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(feature)
+	}
+
+}
+
+func runThird(client pb.RouteGuideClient) {
+	points := []*pb.Point{
+		{Latitude: 313374060, Longitude: 121358540},
+		{Latitude: 311054130, Longitude: 121598790},
+		{Latitude: 310235000, Longitude: 121437403},
+	}
+
+	clientStream, err := client.RecordRoute(context.Background())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, point := range points {
+		if err := clientStream.Send(point); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(time.Second)
+	}
+
+	summary, err := clientStream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(summary)
+
+}
+
+func readIntFromCommandLine(reader *bufio.Reader, target *int32) {
+	_, err := fmt.Fscanf(reader, "%d\n", target)
+	if err != nil {
+		log.Fatalln("Cannot scan", err)
+	}
+}
+
+func runForth(client pb.RouteGuideClient) {
+	stream, err := client.Recommend(context.Background())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// gorountine listen to the server
+	go func() {
+		feature, err2 := stream.Recv()
+
+		if err2 != nil {
+			log.Fatalln(err2)
+		}
+
+		fmt.Println("Recommend: ", feature)
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// new a point for or the point in request would be nil
+		request := pb.RecommendationRequest{Point: new(pb.Point)}
+		var mode int32
+		fmt.Print("Enter recommendation mode: (0 for farthest, 1 for nearest)")
+		readIntFromCommandLine(reader, &mode)
+		fmt.Print("Enter Latitude: ")
+		readIntFromCommandLine(reader, &request.Point.Latitude)
+		fmt.Print("Enter Longitude: ")
+		readIntFromCommandLine(reader, &request.Point.Longitude)
+		request.Mode = pb.RecommendationMode(mode)
+
+		if err := stream.Send(&request); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
 
 }
 
@@ -29,6 +132,5 @@ func main() {
 
 	client := pb.NewRouteGuideClient(conn)
 
-	runFirst(client)
-
+	runForth(client)
 }
--- grpc-example/route-server/server.go
+++ grpc-example/route-server/server.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
+	"io"
 	"log"
+	"math"
 	"net"
+	"time"
 
 	pb "github.com/Azvf/learn-go/grpc-example/route"
 )
@@ -26,15 +29,131 @@ func (s *RouteGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb
 	return nil, nil
 }
 
-func (s *RouteGuideServer) ListFeatures(*pb.Rectangle, pb.RouteGuide_ListFeaturesServer) error {
+func inRange(point *pb.Point, rectangle *pb.Rectangle) bool {
+	left := math.Min(float64(rectangle.Lo.Longitude), float64(rectangle.Hi.Longitude))
+	right := math.Max(float64(rectangle.Lo.Longitude), float64(rectangle.Hi.Longitude))
+	top := math.Max(float64(rectangle.Lo.Latitude), float64(rectangle.Hi.Latitude))
+	bottom := math.Min(float64(rectangle.Lo.Latitude), float64(rectangle.Hi.Latitude))
+
+	if float64(point.Longitude) > left &&
+		float64(point.Longitude) < right &&
+		float64(point.Latitude) > bottom &&
+		float64(point.Latitude) < top {
+		return true
+	}
+
+	return false
+}
+
+func (s *RouteGuideServer) ListFeatures(rectangle *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.features {
+		if inRange(feature.Location, rectangle) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
-func (s *RouteGuideServer) RecordRoute(pb.RouteGuide_RecordRouteServer) error {
+func toRadians(num float64) float64 {
+	return num * math.Pi / float64(180)
+}
+
+// calcDistance calculates the distance between two points using the "haversine" formula.
+// The formula is based on http://mathforum.org/library/drmath/view/51879.html.
+func calcDistance(p1 *pb.Point, p2 *pb.Point) int32 {
+	const CordFactor float64 = 1e7
+	const R = float64(6371000) // earth radius in metres
+	lat1 := toRadians(float64(p1.Latitude) / CordFactor)
+	lat2 := toRadians(float64(p2.Latitude) / CordFactor)
+	lng1 := toRadians(float64(p1.Longitude) / CordFactor)
+	lng2 := toRadians(float64(p2.Longitude) / CordFactor)
+	dlat := lat2 - lat1
+	dlng := lng2 - lng1
+
+	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
+		math.Cos(lat1)*math.Cos(lat2)*
+			math.Sin(dlng/2)*math.Sin(dlng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	distance := R * c
+	return int32(distance)
+}
+
+func (s *RouteGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
+	startTime := time.Now()
+	var pointCount, distance int32
+	var prevPoint *pb.Point
+	for {
+		point, err := stream.Recv()
+
+		if err == io.EOF {
+			// gen a summary and send back
+			endTime := time.Now()
+			return stream.SendAndClose(&pb.RouteSummary{
+				PointCount:  pointCount,
+				Distance:    distance,
+				ElapsedTime: int32(endTime.Sub(startTime).Seconds())},
+			)
+		}
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		pointCount++
+		if prevPoint != nil {
+			distance += calcDistance(prevPoint, point)
+		}
+		prevPoint = point
+	}
 	return nil
 }
 
-func (s *RouteGuideServer) Recommend(pb.RouteGuide_RecommendServer) error {
+func (s *RouteGuideServer) recommendOnce(request *pb.RecommendationRequest) (*pb.Feature, error) {
+	var nearest, farthest *pb.Feature
+	var nearestDistance, farthestDistance int32
+
+	for _, feature := range s.features {
+		distance := calcDistance(feature.Location, request.Point)
+		if nearest == nil || distance < nearestDistance {
+			nearestDistance = distance
+			nearest = feature
+		}
+		if farthest == nil || distance > farthestDistance {
+			farthestDistance = distance
+			farthest = feature
+		}
+	}
+	if request.Mode == pb.RecommendationMode_GetFarthest {
+		return farthest, nil
+	} else {
+		return nearest, nil
+	}
+}
+
+func (s *RouteGuideServer) Recommend(stream pb.RouteGuide_RecommendServer) error {
+	for {
+		request, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		recommended, err2 := s.recommendOnce(request)
+
+		if err2 != nil {
+			return err
+		}
+
+		return stream.Send(recommended)
+	}
+
 	return nil
 }
 
